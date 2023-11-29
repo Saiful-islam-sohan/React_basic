3e005a4bc0a4ffadbaccd9f1b4c7f71be1196c8c
Saiful Islam sohan <saifulislam.cse.pust@gmail.com>
2023-11-29 20:52:11 +0600

    cara

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,32 @@
-
+import './Cart.css'
 
 const Cart = (props) => {
+   const {cart}=props
     // console.log(props);
     // {length} = {props.cart}
+
+    let total=0;
+    let shipping=0;
+   
+
+    for(const product of cart){
+        total =total+product.price;
+        shipping=shipping+product.shipping;
+
+    }
   
+    const tex=  (total*0.1).toFixed(2);
+
+    const grandTotal= total +shipping+Number(tex);
   
     return (
-        <div>
+        <div className="cart"> 
              <h3>Order Summery</h3>
-                  <p>selected items:{props.cart.length}</p>
+            <p>selected items:{cart.length}</p>
+            <p>Total price:{total}</p>
+            <p>total shipping:{shipping}</p>
+            <p>Tex:{tex}</p>
+            <h3>Grand Total:{grandTotal}</h3>
         </div>
     );
 };
